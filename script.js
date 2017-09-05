107e45114a0eb8ad69dba6fabf656ff26988d4c4
yellow544 <31159133+yellow544@users.noreply.github.com>
2017-09-05 22:05:24 +0100

    if NaN added

--- script.js
+++ script.js
@@ -3,17 +3,27 @@
 var a = prompt("Enter number for a"),
 		b = prompt("Enter number for b")
 		value = (a * a) + (2 * a * b) - (b * b)
-
-if (value < 0) {
+		
+if (isNaN(value)) {
+		alert("Please enter numbers only!")
+		console.log("Please enter numbers only!")
+		
+}	else if (value < 0) {
 		console.log("The value is below zero");
+		
 		alert("The value is below 0");
+		
 }	else if (value > 0) {
 		console.log("The value is above 0");
 		alert("The value is above 0");
+		
 }	else if (value == 0) {
 		console.log("The value is 0");
-		alert("The value is 0");  
+		alert("The value is 0");
+		
 }	else {
 		console.log("This is an unknown value");
 		alert("This is an unknown value")
 }
+
+	
